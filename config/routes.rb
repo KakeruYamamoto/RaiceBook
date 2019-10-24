73da0f24478898a109962c47fdca501627abc119
kakeru <okinawan.kkr0713@icloud.com>
2019-10-24 13:02:11 +0900

    view/new.html変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :feeds do
