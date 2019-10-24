dffd2bb079bfac53e67767c6a64b3bbe0bb61f4e
kakeru <okinawan.kkr0713@icloud.com>
2019-10-24 13:07:46 +0900

    controller/createに条件分岐を追記

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,13 @@
 class SessionsController < ApplicationController
   def new
   end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ログイン成功した場合
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render 'new'
+    end
+  end
 end
