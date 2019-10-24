2cd67a9c92b9aab1139f294bc0af11ea9b8eacc3
kakeru <okinawan.kkr0713@icloud.com>
2019-10-24 12:58:33 +0900

    controller/session＆view/new.html生成、

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :feeds do
