b4f21a3fa6d82c9e31c9de5dbf7f5c9b66adda60
kakeru <okinawan.kkr0713@icloud.com>
2019-10-24 11:48:38 +0900

    routes追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
   resources :feeds do
     collection do
       post :confirm
-      patch :confirm
     end
   end
 end
