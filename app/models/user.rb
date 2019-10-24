6dd210658798751b263d6dfd2effcca444c6a185
kakeru <okinawan.kkr0713@icloud.com>
2019-10-24 14:35:21 +0900

    migrateファイル生成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :feeds
   validates :name,  presence: true, length: {maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: true,
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
