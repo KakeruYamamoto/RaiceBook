cd34460ee3cca661f233e52bbab36c29236e2b8c
kakeru <okinawan.kkr0713@icloud.com>
2019-10-24 13:42:05 +0900

    destroy追記

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
+  
   def new
   end
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
@@ -10,5 +12,11 @@ class SessionsController < ApplicationController
       flash.now[:danger] = 'ログインに失敗しました'
       render 'new'
     end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
   end
 end
