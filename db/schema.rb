ee03bc50d647094f7201d95565decda7b9d870c2
kakeru <okinawan.kkr0713@icloud.com>
2019-10-24 10:39:46 +0900

    schemaファイル更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_22_110058) do
+ActiveRecord::Schema.define(version: 2019_10_24_013116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "feeds", force: :cascade do |t|
     t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
