6e276efd4be2c1bf0ffb3786845808388cd5997a
kakeru <okinawan.kkr0713@icloud.com>
2019-10-25 17:08:22 +0900

    test commit

--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,6 @@ module RaiceBook
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.assets.initialize_on_precompile = false
   end
 end
