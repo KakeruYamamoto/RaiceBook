00fcc0a59e8e922b92adc84bcdc372385ebe0a10
kakeru <okinawan.kkr0713@icloud.com>
2019-10-28 14:58:22 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'users#new'
+  root to: "users#new"
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :feeds do
