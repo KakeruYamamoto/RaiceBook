8831a9f782e20d96432c92cd19fa86fd4dc7b341
kakeru <okinawan.kkr0713@icloud.com>
2020-04-09 23:47:26 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: 'users#new'
+  root to: 'feeds#index'
   resources :sessions, only: %i[new create destroy]
   resources :users, only: %i[new create show]
   resources :feeds do
